24b956f45db707fbaa7448ca2ddfba2bbec5ca54
clach04 <clach04@gmail.com>
2026-02-08 12:06:38 -0800

    Attempt to fix https://github.com/pebble-dev/pebblejs/issues/20

--- waftools/aplite_legacy.py
+++ waftools/aplite_legacy.py
@@ -3,7 +3,7 @@ from waflib.Configure import conf
 
 @conf
 def appinfo_bitmap_to_png(ctx, appinfo_json):
-    if not ctx.supports_bitmap_resource():
+    if hasattr(ctx, 'supports_bitmap_resource') and not ctx.supports_bitmap_resource():
         for res in appinfo_json['resources']['media']:
             if res['type'] == 'bitmap':
                 res['type'] = 'png'
